2df2376c0ccc1e3855407527cc605b73c66590f2
adnanshaik <shaikhadnan6608@gmail.com>
2020-09-12 02:17:44 +0530

    Added:Doublet combination as well as percentage

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,35 +1,51 @@
-count=0
-while [ $count -lt 5 ]
+
+#!/bin/bash -x
+read -p "enter num:" n
+declare -A dict					#declaration of dictionary 
+hh=0
+tt=0
+ht=0
+th=0
+count=1
+while [ $count -le $n ]
 do
-        r=$((RANDOM%2))
-dict[$r]=$r
-        if [ $r -eq 0 ]
+        r1=$((RANDOM%2))
+        r2=$((RANDOM%2))
+dict[$r1$r2]=$r1$r2				
+        if [ $r1 -eq 0 ] && [ $r2 -eq 0 ]
         then
-                ((head++))
-                dict[$r]=$head
-        else
-                ((tail++))
-                dict[$r]=$tail
+               ((hh++))
+                dict[$r1$r2]=$hh
+        elif [ $r1 -eq 1 ] && [ $r2 -eq 1 ]
+        then
+                ((tt++))
+                 dict[$r1$r2]=$tt
+        elif [ $r1 -eq 0 ] && [ $r2 -eq 1 ]
+        then
+                ((ht++))
+                 dict[$r1$r2]=$ht
+         elif [ $r1 -eq 1 ] && [ $r2 -eq 0 ]
+        then
+                ((th++))
+                 dict[$r1$r2]=$th
         fi
 ((count++))
 done
-for i in ${dict[@]}
-do
-        echo $i
-done
-echo "head=" $head "tail="$tail
-if [ $head -ge 3 ]
+echo ${dict[@]}
+if [ $hh -gt $tt ] && [ $hh -gt $th ] && [ $hh -gt $ht ]
 then
-        per=$(( head*100/5 ))
-        echo "head="$per
-elif [ $tail -gt 3 ]
+        per=$(( hh*100/n ))
+        echo "winner is HH="$per
+elif [ $tt -gt $hh ] && [ $tt -gt $th ] && [ $tt -gt $ht ]
         then
-               per=$(( tail*100/5 ))
-        echo "tail="$per
-elif [ $head -eq $tail ]
+               per=$(( tt*100/n ))
+        echo "winner is TT="$per
+elif [ $th -gt $tt ] && [ $th -gt $hh ] && [ $th -gt $ht ]
 then
-                perofhead=$(( head*100/5 ))
-               peroftail=$(( tail*100/5 ))
-                echo "head="$perofhead"and tail="$peroftail
+               per=$(( th*100/n ))
+                echo "winner is TH="$per
+elif [ $ht -gt $tt ] && [ $th -gt $hh ] && [ $th -gt $th ]
+then
+                per=$(( ht*100/n ))
+                echo "winner is HT="$per
 fi
-
